915cb688a4c94c70859b23cf34cefadd5fc5e52e
Matteo De Gobbi <matteodegobbi2001@gmail.com>
2023-03-14 16:08:05 +0100

    htonl compatibile sia con windows che con linux

--- Reti/hton/hton.c
+++ Reti/hton/hton.c
@@ -19,28 +19,36 @@ short htons(short n){
     *(pt+1)=temp;
     return n;
 }
+/*CONVERTE UN LONG IN RAPPRESENTAZIONE BIG ENDIAN SE NON LO E' GIA', FUNZIONA SUA SU LINUX CHE SU WINDOWS*/
 long htonl(long n){
+    int nByteLong=sizeof(long);
     if (!isLittleEndian){
         return n;
     }
     char* pt=(char*)&n;
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < nByteLong/2; i++){
         char temp=*pt;
-        *(pt+i)=*(pt+7-i);
-        *(pt+7-i)=temp;
+        *(pt+i)=*(pt+nByteLong-1-i);
+        *(pt+nByteLong-1-i)=temp;
     }
     return n;
 }
+
 #include <stdio.h>
 #include <math.h>
+/*main di test per htons e htonl*/
 int main(int argc, char const *argv[]){
-    /*unsigned short n=pow(2,16)-1;
-    printf("%d \n",n);
-    printf("%d \n",htons(n));
+    /*00000000 00000001 -> 00000001 00000000 short e' 2 byte sia su Windows che su Linux*/
+    unsigned short n=1;
+    printf("short little endian:%lu \n",n);
+    printf("short big endian:%lu \n",htons(n));
 
-    return 0;*/
-    //00000001 00000001 00000001 00000001 
-    unsigned long n=128;
-    printf("%lu \n",n);
-    printf("%lu \n",htonl(n));
+    return 0;
+
+    /*00000000 00000000 00000000 00000001 -> 00000001 00000000 00000000 00000000 su Windows long e' 4 byte*/
+    /*00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000001 -> 
+    -> 00000000 00000000 00000000 00000000 00000001 00000000 00000000 00000000 su Linux long e' 8 byte*/
+    unsigned long n=1;
+    printf("long little endian:%lu \n",n);
+    printf("long big endian:%lu \n",htonl(n));
 }
